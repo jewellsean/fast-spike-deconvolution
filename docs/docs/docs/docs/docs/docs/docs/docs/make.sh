f324771bbd2d3d7d051a5d2f621a896e86dc396e
Sean Jewell <jewellsean@gmail.com>
2018-12-10 10:04:50 -0800

    remove docs in docs folder recursion

--- docs/docs/docs/docs/docs/docs/docs/docs/make.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-Rscript build.R _source/tutorial.Rmd;
-jekyll build
-jekyll serve
--- docs/docs/docs/docs/docs/docs/docs/make.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-Rscript build.R _source/tutorial.Rmd;
-jekyll build
-rm -rf docs
-cp -r _site docs
-jekyll serve
